60dd0b718da43843d252fe7186437ae617db84e5
Jerome Haas <hello@jeromehaas.dev>
2022-01-28 16:00:15 +0100

    Refactor: change .env to heroku defaults

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const router = require('./router');
 require('dotenv').config();
-const port = provess.env.APP_PORT;
+const port = process.env.PORT;
 
 
 app.use(cors());
@@ -14,6 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(port || 3001, () => {
+app.listen(port || process.env.PORT || 3001, () => {
 	console.log(`🚀 App is up and running on port ${port}...`);
 })
